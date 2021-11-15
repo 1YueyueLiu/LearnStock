fed00abcbf098290ddd4ffa5b462ea76d127abd4
Yueyue Liu <2515474L@student.gla.ac.uk>
2021-11-15 15:43:04 +0000

    Mapping URLs

--- stock/views.py
+++ stock/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+def index(request):
+    return HttpResponse(" Stock has the high return")
+
+def about(request):
+    return HttpResponse("Stock says here is some information about this app")
