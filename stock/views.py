d56b338401ed9c7dbb74cbe6422d935695e7d958
Yueyue Liu <2515474L@student.gla.ac.uk>
2021-11-19 17:01:16 +0000

    The function of Login has been finished

--- stock/views.py
+++ stock/views.py
@@ -7,6 +7,8 @@ from stock.forms import CategoryForm, PageForm
 from django.shortcuts import redirect
 from django.urls import reverse
 from stock.forms import UserForm, UserProfileForm
+from django.contrib.auth import authenticate,login
+
 
 
 # Create your views here.
@@ -116,3 +118,20 @@ def register(request):
                        'profile_form':profile_form,
                        'registered':registered})
 
+def user_login(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(username=username,password=password)
+
+        if user:
+            if user.is_active:
+                login(request,user)
+                return redirect(reverse('stock:index'))
+            else:
+                return HttpResponse("Your account is disabled.")
+        else:
+            print(f"Invalid login details:{username},{password}")
+            return HttpResponse("Invalid login details supplied")
+    else:
+        return render(request,'stock/login.html')
